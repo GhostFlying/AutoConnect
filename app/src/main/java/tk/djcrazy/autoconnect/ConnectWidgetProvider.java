9ae6c8eb918a5b7a94ad4e69cb302389528c3ddb
GhostFlying <GhostFlying@Gmail.com>
2014-05-11 16:23:35 +0800

    change the widget

--- app/src/main/java/tk/djcrazy/autoconnect/ConnectWidgetProvider.java
+++ app/src/main/java/tk/djcrazy/autoconnect/ConnectWidgetProvider.java
@@ -1,32 +1,37 @@
 package tk.djcrazy.autoconnect;
 
-import java.util.List;
+
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 public class ConnectWidgetProvider extends AppWidgetProvider {
 	
 	final String TAG = this.getClass().getName();
+    static int[] ids = null;
 
 	
 	@Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.d(TAG, "onUpdate(): appWidgetIds.length="+appWidgetIds.length);
         RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.connect_widget_layout);
-        remoteView.setOnClickPendingIntent(R.id.connect_button, getPendingIntent(context));
-        for (int id: appWidgetIds) {
-        	appWidgetManager.updateAppWidget(id, remoteView);
+        remoteView.setOnClickPendingIntent(R.id.imageButton, getPendingIntent(context));
+        WifiManager mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (mWifiManager.isWifiEnabled() && mWifiManager.getConnectionInfo().getSSID().equalsIgnoreCase("\"ZJUWLAN\"")) {
+            remoteView.setImageViewResource(R.id.imageButton, R.drawable.open);
         }
+        else {
+            remoteView.setImageViewResource(R.id.imageButton, R.drawable.close);
+        }
+        ids = appWidgetIds;
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteView);
     }
 	
     @Override  
@@ -34,33 +39,28 @@ public class ConnectWidgetProvider extends AppWidgetProvider {
         super.onReceive(context, intent);  
         Log.d(TAG, "onReceive start." + intent);
         if (intent.hasCategory(Intent.CATEGORY_ALTERNATIVE)) {
-        	 WifiManager mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);         	 
-        	 
-        	 WifiConfiguration zjuwlan = new WifiConfiguration();
-        	 zjuwlan.SSID = "\"ZJUWLAN\"";
-        	 zjuwlan.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-        	 while (!mWifiManager.isWifiEnabled()) {
-        		 mWifiManager.setWifiEnabled(true);
-        	 }
-        	 mWifiManager.disconnect();
-        	 int netid = mWifiManager.addNetwork(zjuwlan);
-        	 mWifiManager.enableNetwork(netid, true);
-        	 mWifiManager.reconnect();
-        	 List<WifiConfiguration> wifiList = mWifiManager.getConfiguredNetworks();
-        	 Log.d(TAG, "total count :" + wifiList.size());
-        	 /*List<WifiConfiguration> wifiList = mWifiManager.getConfiguredNetworks();
-        	 Log.d(TAG, "total count :" + wifiList.size());
-        	 mWifiManager.disconnect();
-        	 for (WifiConfiguration each :wifiList) {      
-        		 Log.d(TAG, "SSID :" + each.SSID);
-        		 if (each.SSID.replace("\"", "").equals("ZJUWLAN")) {
-        			 mWifiManager.enableNetwork(each.networkId, true);
-        			 mWifiManager.reconnect();
-        			 return;
-        		 }
-        	 }
-        	 Toast.makeText(context, context.getString(R.string.please_connect_once), Toast.LENGTH_SHORT).show();
-        	 Log.e(TAG, "ZJUWLAN is not stored");        	 */
+            RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.connect_widget_layout);
+            remoteView.setOnClickPendingIntent(R.id.imageButton, getPendingIntent(context));
+        	WifiManager mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            if (mWifiManager.isWifiEnabled() && mWifiManager.getConnectionInfo().getSSID().equalsIgnoreCase("\"ZJUWLAN\"")) {
+                mWifiManager.setWifiEnabled(false);
+                remoteView.setImageViewResource(R.id.imageButton, R.drawable.close);
+            }
+            else {
+                WifiConfiguration zjuwlan = new WifiConfiguration();
+                zjuwlan.SSID = "\"ZJUWLAN\"";
+                zjuwlan.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                while (!mWifiManager.isWifiEnabled()) {
+                    mWifiManager.setWifiEnabled(true);
+                }
+                mWifiManager.disconnect();
+                int netId = mWifiManager.addNetwork(zjuwlan);
+                mWifiManager.enableNetwork(netId, true);
+                mWifiManager.reconnect();
+                remoteView.setImageViewResource(R.id.imageButton, R.drawable.open);
+            }
+            AppWidgetManager mAppWidget= AppWidgetManager.getInstance(context);
+            mAppWidget.updateAppWidget(ids, remoteView);
         }
     }  
 	
