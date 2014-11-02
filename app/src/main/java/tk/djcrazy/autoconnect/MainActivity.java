37fb9307015cb047367ef8b726ce5dd41785851c
GhostFlying <GhostFlying@Gmail.com>
2014-11-02 17:48:15 +0800

    Change the theme to AppCompat

--- app/src/main/java/tk/djcrazy/autoconnect/MainActivity.java
+++ app/src/main/java/tk/djcrazy/autoconnect/MainActivity.java
@@ -6,30 +6,31 @@ import roboguice.inject.InjectView;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-@ContentView(R.layout.activity_main)
-public class MainActivity extends RoboActivity {
+public class MainActivity extends ActionBarActivity {
 
 	public static final String USER_INFO = "user_info";
 	public static final String USER_NAME = "user_name";
 	public static final String PASSWORD = "password";
 	public static final String LAST_LOGIN_TIME = "last_login_time";
-	
-	
-	@InjectView(R.id.username)
+
+
 	private EditText userName;
-	@InjectView(R.id.password)
 	private EditText password;
-	@InjectView(R.id.sure)
 	private Button sure;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userName = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        sure = (Button)findViewById(R.id.sure);
         SharedPreferences userInfo = getSharedPreferences("user_info", MODE_PRIVATE);  
         userName.setText( userInfo.getString("user_name", ""));
         password.setText( userInfo.getString("password", ""));
